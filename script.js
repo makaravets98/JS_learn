58d71aacfc75f0cd319ab60007ebbba862da9fd3
makaravets98 <mmakarovets@gmail.com>
2023-07-18 22:21:00 +0200

    feat: done step_2

--- script.js
+++ script.js
@@ -1,41 +1,86 @@
 /* Задание на урок:
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
-Проверить, чтобы все работало без ошибок в консоли */
+4) Потренироваться и переписать цикл еще двумя способами*/
+// Код возьмите из предыдущего домашнего задания
 'use strict';
-const numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMovieDB = {
-    count: numberofFilms,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     private: false
 };
 
-const question_1 = prompt('Один из последних просмотренных фильмов?', '');
-const question_2 = +prompt('На сколько оцените его?', '');
-const question_3 = prompt('Один из последних просмотренных фильмов?', '');
-const question_4 = +prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[question_1] = question_2;
-personalMovieDB.movies[question_3] = question_4;
-
+for (let i = 0; i< 2; i++){
+let question_1 = prompt('Один из последних просмотренных фильмов?', '');
+let question_2 = +prompt('На сколько оцените его?', '');
+    if ( question_1 != null && question_2 != null && question_1 != '' && question_2 != '' && question_1.length<50){
+        personalMovieDB.movies[question_1] = question_2;
+        alert('Done');
+    }
+    else{
+        alert('Error');
+        i--;
+    }
+}
+if ( personalMovieDB.count < 10 ){
+    alert('Просмотрено довольно мало фильмов');
+}
+else if ( personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ){
+    alert('Вы классический зритель');
+}
+else if (personalMovieDB.count > 30){
+    alert('Вы - киноман');
+}
+else {
+    alert('Произошла ошибка');
+}
 console.log(personalMovieDB);
+
+
+
+// Цикл while
+
+// let i = 0;
+// while (i<2){
+//     let question_1 = prompt('Один из последних просмотренных фильмов?', '');
+// let question_2 = +prompt('На сколько оцените его?', '');
+//     if ( question_1 != null && question_2 != null && question_1 != '' && question_2 != '' && question_1.length<50){
+//         personalMovieDB.movies[question_1] = question_2;
+//         alert('Done');
+//         i++;
+//     }
+//     else{
+//         alert('Error');
+//         i--;
+//     }
+// }
+
+// Цикл do while
+
+// let i = 0;
+// do{
+//     let question_1 = prompt('Один из последних просмотренных фильмов?', '');
+//     let question_2 = +prompt('На сколько оцените его?', '');
+//         if ( question_1 != null && question_2 != null && question_1 != '' && question_2 != '' && question_1.length<50){
+//             personalMovieDB.movies[question_1] = question_2;
+//             alert('Done');
+//             i++;
+//         }
+//         else{
+//             alert('Error');
+//             i--;
+//         }
+// }
+// while( i < 2 );
